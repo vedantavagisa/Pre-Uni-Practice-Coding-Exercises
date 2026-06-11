8dd5785155a75450cdbc6e31845021f85c0fab25
vedantavagisa <vedantavagisa1486@gmail.com>
2026-06-11 14:28:47 -0400

    aaaaaa lol hi idk what to put here

---
+++ Greatest Integer in Matrix/src/matrixSums.java
@@ -0,0 +1,22 @@
+public class matrixSums {
+    public static void main(String[] args) {
+
+        System.out.println("Array:");
+        System.out.println("{");
+        double[][] arr = new double[3][3];
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                arr[i][j] = Math.random();
+                System.out.print(arr[i][j] + ", ");
+            } System.out.println();
+        } System.out.println("}");
+
+        double sum = 0;
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                sum += arr[j][i];
+            } System.out.println("Column " + i + " sum: " + sum);
+            sum = 0;
+        } 
+    }
+}
