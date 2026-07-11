2b2fcc4adaec9770d28d23bdc2e574d7dd6ea31d
vedantavagisa <vedantavagisa1486@gmail.com>
2026-07-10 21:15:02 -0400

    Updating my BinarySearch iterative algorithm with GPT's suggestions.

--- Day 9 - Binary Search/Iterative BinarySearch/src/Main.java
+++ Day 9 - Binary Search/Iterative BinarySearch/src/Main.java
@@ -4,18 +4,19 @@ import java.util.Scanner;
 public static int binarySearch(int[] arr, int target) {
     int left = 0;
     int right = arr.length - 1;
-    while (left != right) {
-        int middle = (left + right)/2;
+    while (left <= right) { //I initially had left==right, but GPT suggested that I replace it with what I have right now
+        int middle = (left + right)/2;  //GPT suggested that I use int middle = left + (right - left) / 2; for large ints to avoid overflowing
         if (arr[middle] == target) {
             return middle;
         } else if (arr[middle] > target) {
             right = middle - 1;
         } else if (arr[middle]< target) {
             left = middle + 1;
-        } if (target > arr[right] || target < arr[left]) {
+        } /* What I had initially when I coded it was this:
+        if (target > arr[right] || target < arr[left]) {
             return -1;
-        }
-    } return -1;
+        }* This is unnecessary because the left<= right condition already handles anything that would lead to that case
+*/  } return -1;
 } public static void main(String[] args) {
     int[] arr = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
     Scanner sc = new Scanner(System.in);
