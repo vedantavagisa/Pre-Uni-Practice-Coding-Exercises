05069ba216043e0118e49b4f6278d4705c75dfe0
vedantavagisa <vedantavagisa1486@gmail.com>
2026-07-10 21:00:08 -0400

    Amending the rest of Day 9

--- Day 9 - Binary Search/Iterative BinarySearch/src/Main.java
+++ Day 9 - Binary Search/Iterative BinarySearch/src/Main.java
@@ -1,6 +1,6 @@
 /**
  * Purpose: Familiarize myself with binarySearch - this project does so in an iterative way. */
-
+import java.util.Scanner;
 public static int binarySearch(int[] arr, int target) {
     int left = 0;
     int right = arr.length - 1;
@@ -9,11 +9,17 @@ public static int binarySearch(int[] arr, int target) {
         if (arr[middle] == target) {
             return middle;
         } else if (arr[middle] > target) {
-            
+            right = middle - 1;
+        } else if (arr[middle]< target) {
+            left = middle + 1;
+        } if (target > arr[right] || target < arr[left]) {
+            return -1;
         }
     } return -1;
-}
-
-public static void main(String[] args) {
-    IO.println(String.format("Hello and welcome!"));
+} public static void main(String[] args) {
+    int[] arr = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+    Scanner sc = new Scanner(System.in);
+    System.out.println("The array consists of the first 10 positive integers from 0 to 9 inclusive.");
+    System.out.println("Please input the number whose index you would like to search.");
+    System.out.println(binarySearch(arr, sc.nextInt()));
 }
