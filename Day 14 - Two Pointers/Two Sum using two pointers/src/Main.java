f9dd4ff681b14c15672b2e99eb3cab472c408d44
vedantavagisa <vedantavagisa1486@gmail.com>
2026-07-28 14:18:34 -0400

    Added in the purpose statement of the project, commented out the recursive method
    Recursion was fun, but I put it under the "purpose" section since twoSum is meant to be iterative.

--- Day 14 - Two Pointers/Two Sum using two pointers/src/Main.java
+++ Day 14 - Two Pointers/Two Sum using two pointers/src/Main.java
@@ -1,20 +1,27 @@
-public static int[] twoSum(int[] arr, int target, int left, int right) {
-    int[] ans = {-1, -1};
-    int sum = arr[left] + arr[right];
-    if (left >= right) {
-        return ans;
-    } if (sum > target) {
-        return twoSum(arr, target, left, right - 1);
-    } else if (sum < target) {
-        return twoSum(arr, target, left + 1, right);
-    } else {
-        ans[0] = left;
-        ans[1] = right;
-        return ans;
-    }
-}
+/**
+ * Purpose: Solve the famous two-sum problem using two pointers.
+ * Note: I love recursion so I decided to write the method recursively as follows:
 
-/**public static int[] twoSum(int[] arr, int target) {
+     public static int[] twoSum(int[] arr, int target, int left, int right) {
+         int[] ans = {-1, -1};
+         int sum = arr[left] + arr[right];
+         if (left >= right) {
+            return ans;
+         } if (sum > target) {
+            return twoSum(arr, target, left, right - 1);
+         } else if (sum < target) {
+            return twoSum(arr, target, left + 1, right);
+         } else {
+             ans[0] = left;
+             ans[1] = right;
+             return ans;
+         }
+     }
+
+ * */
+
+
+public static int[] twoSum(int[] arr, int target) {
     int[] ans = {-1, -1};
     int left = 0;
     int right = arr.length - 1;
@@ -30,12 +37,12 @@ public static int[] twoSum(int[] arr, int target, int left, int right) {
             return ans;
         }
     } return ans;
-}*/
+}
 
 public static void main(String[] args) {
     int[] arr = {1, 3, 4, 6, 8, 11, 15};    //change the array here
     int target = 10;     //change the target here
-    int[] ans = twoSum(arr, target, 0, arr.length - 1);
+    int[] ans = twoSum(arr, target);
     if (ans[0]==-1) {
         System.out.println("There are no integers that sum up to " + target);
     } else {
