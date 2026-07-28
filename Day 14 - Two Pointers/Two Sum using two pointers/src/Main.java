f23d78921c9627e74a28b8def1d1b7498c5756f5
vedantavagisa <vedantavagisa1486@gmail.com>
2026-07-28 14:06:29 -0400

    Tried the two-sum problem :D
    This was very fun

---
+++ Day 14 - Two Pointers/Two Sum using two pointers/src/Main.java
@@ -0,0 +1,27 @@
+public static int[] twoSum(int[] arr, int target) {
+    int[] ans = {-1, -1};
+    int left = 0;
+    int right = arr.length - 1;
+    while (left < right) {
+        if (arr[left] + arr[right] > target) {
+            right--;
+        } else if (arr[left] + arr[right] < target) {
+            left++;
+        } else {
+            ans[0] = left;
+            ans[1] = right;
+            return ans;
+        }
+    } return ans;
+}
+
+public static void main(String[] args) {
+    int[] arr = {1, 3, 4, 6, 8, 11, 15};    //change the array here
+    int target = 0;     //change the target here
+    int[] ans = twoSum(arr, target);
+    if (ans[0]==-1) {
+        System.out.println("There are no integers that sum up to " + target);
+    } else {
+        System.out.println("Indices " + ans[0] + " and " + ans[1] + " sum up to " + target);
+    }
+}
