71cb75ce2dbd62e873554c72010c565f1a36b93c
vedantavagisa <vedantavagisa1486@gmail.com>
2026-07-28 12:47:31 -0400

    Two pointers Exercise 1 (Recursive)

---
+++ Recursively reverse Array with two pointers/src/Main.java
@@ -0,0 +1,22 @@
+/**
+ * Purpose: Reverse an array IN PLACE using two pointers. I chose to do this recursively, the iterative version is also available.
+ * */
+
+public static int[] reverse(int[] arr, int i) {
+    if (i >= arr.length - i - 1) {
+        return arr;
+    } int left = arr[i];
+    int right = arr[arr.length - i - 1];
+    arr[i] = right;
+    arr[arr.length - i - 1] = left;
+    reverse(arr, i+1);
+    return arr;
+}
+
+public static void main(String[] args) {
+    int[] arr = {1, 2, 3, 4, 5};
+    reverse(arr, 0);
+    for (int i = 0; i < arr.length; i++) {
+        System.out.println(arr[i]);
+    }
+}
