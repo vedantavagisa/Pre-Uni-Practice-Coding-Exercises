7fd85309bb2ca3ba54f4fe1f58f296e8b0d0b961
vedantavagisa <vedantavagisa1486@gmail.com>
2026-07-29 03:08:40 -0400

    Exercise 4 (incomplete work)
    I got some conceptual thinking, but this is very primitive as of yet. I'll come back to it later.

---
+++ Day 15 - Sliding Window basics/Exercise 4 - First Negative number in subarray/src/Main.java
@@ -0,0 +1,20 @@
+/**
+ * Purpose: Using a sliding window, output the first negative number in each window of size k
+ *
+ * My thought process (done with GPT) tonight happened to be:
+ * Sliding window state:
+ * - Exercise 1-3: store a number (sum/average)
+ * - Exercise 4: store candidates + their positions
+ * - Need FIFO behaviour -> queue
+ * */
+
+public static int[] slidingWindow(int[] arr, int k) {
+    int[] negatives = new int[arr.length - k + 1];
+    int temporary;
+    int i = 0;
+    return negatives;
+}
+
+public static void main(String[] args) {
+
+}
