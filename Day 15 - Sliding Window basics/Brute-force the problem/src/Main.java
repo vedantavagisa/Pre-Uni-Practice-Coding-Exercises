16b828d3ee51eedd67a95cbb051b8ee7a5c4fdd5
vedantavagisa <vedantavagisa1486@gmail.com>
2026-07-29 01:28:26 -0400

    Updated the previous commit with a solution after ACTUALLY reading the problem

--- Day 15 - Sliding Window basics/Brute-force the problem/src/Main.java
+++ Day 15 - Sliding Window basics/Brute-force the problem/src/Main.java
@@ -25,6 +25,20 @@
 //    } return sum;
 //}
 
+
+public static int slidingWindow(int[] arr, int k) {
+    int sum = 0;
+    int tempSum = 0;
+    for (int i = 0; i <= arr.length - k; i++) {
+        tempSum = 0;
+        for (int j = 0; j < k; j++) {
+            tempSum += arr[i+j];
+        } if (tempSum > sum) {
+            sum = tempSum;
+        }
+    } return sum;
+}
+
 public static void main(String[] args) {
     int[] arr = {2, 1, 5, 1, 3, 2};
     int k = 3;
