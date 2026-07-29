dc577130550b3322255f3fe5e23ad0cf6964940b
vedantavagisa <vedantavagisa1486@gmail.com>
2026-07-29 02:05:54 -0400

    Amended the average calculation to be less clunky

--- Day 15 - Sliding Window basics/Exercise 3 - Compute the largest average in an array/src/Main.java
+++ Day 15 - Sliding Window basics/Exercise 3 - Compute the largest average in an array/src/Main.java
@@ -2,15 +2,17 @@
  * Purpose: find the sum of a contiguous subarray with k elements, but do this in a proper sliding window fashion, i.e. don't recompute sums.
  * */
 
-public static double slidingWindow(int[] arr, double k) {
+public static double slidingWindow(int[] arr, int k) {
     double avg = Double.MIN_VALUE;
     double tempAvg = 0;
+    double sum = 0;
     for (int i = 0; i < k; i++) {
-        tempAvg += (1/k)*arr[i];
-    } avg = tempAvg;
+        sum += arr[i];
+    } tempAvg = sum/k;
+    avg = tempAvg;
     for (int i = 0; i < arr.length - k; i++) {
-        tempAvg += arr[i + (int)k]*(1/k);
-        tempAvg -= arr[i]*(1/k);
+        sum = sum + arr[i + k] - arr[i];
+        tempAvg = sum/k;
         if (tempAvg > avg) {
             avg = tempAvg;
         }
