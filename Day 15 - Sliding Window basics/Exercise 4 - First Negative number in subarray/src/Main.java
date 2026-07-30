beb47ed710f68bf130f760a1fc12fe3236c2be22
vedantavagisa <vedantavagisa1486@gmail.com>
2026-07-30 17:02:43 -0400

    Finished Exercise 4!
    I also learned how queues worked.

--- Day 15 - Sliding Window basics/Exercise 4 - First Negative number in subarray/src/Main.java
+++ Day 15 - Sliding Window basics/Exercise 4 - First Negative number in subarray/src/Main.java
@@ -1,20 +1,49 @@
 /**
  * Purpose: Using a sliding window, output the first negative number in each window of size k
- *
- * My thought process (done with GPT) tonight happened to be:
- * Sliding window state:
- * - Exercise 1-3: store a number (sum/average)
- * - Exercise 4: store candidates + their positions
- * - Need FIFO behaviour -> queue
- * */
+ * This was cool! I learned how queues worked.
+ */
+
 
 public static int[] slidingWindow(int[] arr, int k) {
     int[] negatives = new int[arr.length - k + 1];
-    int temporary;
-    int i = 0;
-    return negatives;
+    Queue<Integer> queue = new LinkedList<>();
+    int leftBoundary;
+    int index;
+    for (int j = 0; j < k; j++) {
+        if (arr[j] < 0) {
+            queue.add(j);
+        } if (queue.isEmpty()) {
+            negatives[j] = 0;
+        } else {
+            negatives[j] = arr[queue.peek()];
+        }
+    } for (int i = k; i < negatives.length; i++) {
+        leftBoundary = i - k + 1;
+        if (arr[i] < 0) {
+            queue.add(i);
+        } if (queue.isEmpty()) {
+            negatives[i] = 0;
+        } else {
+            index = queue.peek();
+            if (index < leftBoundary) {
+                queue.remove();
+                if (queue.isEmpty()) {
+                    negatives[i] = 0;
+                } else {
+                    negatives[i] = arr[queue.peek()];
+                }
+            } else {
+                negatives[i] = arr[queue.peek()];
+            }
+        }
+    } return negatives;
 }
 
 public static void main(String[] args) {
-
+    int[] arr = {12, -1, -7, 8, -15, 30, 16, 28}; //12, -1, -7, 8, -15, 30, 16, 28
+    int k = 3;
+    int[] negatives = slidingWindow(arr, k);
+    for (int i = 0; i < negatives.length; i++) {
+        System.out.println(negatives[i]);
+    }
 }
