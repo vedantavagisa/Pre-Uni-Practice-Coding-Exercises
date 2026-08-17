23e49d8eedc9f6941f7df4ccbf00f4d2feabb1b0
vedantavagisa <vedantavagisa1486@gmail.com>
2026-08-17 16:48:29 -0400

    Cleaned up Recursion Exercise 3
    Removed comments containing both the Java algorithm and the algorithm from yesterday.

--- Day 21 - Python fluency/Recursion/Recursion Exercise 3.py
+++ Day 21 - Python fluency/Recursion/Recursion Exercise 3.py
@@ -1,23 +1,5 @@
 def permutations(string, passemot):
-    '''initial thoughts:
-    for i in range(len(string)):
-        if (i != n):
-            passemot = f"{passemot}{string[i]}"
-            print(passemot)
-    if (n < len(string)-1):
-        permutations(n+1, string, f"{passemot}{string[n+1]}")
 
-    what i did for the java method:
-        if (ogStr.length()==1) {
-        System.out.println(output + ogStr);
-        } else {
-            for (int i = 0; i < ogStr.length(); i++) {
-                generatePermutations(ogStr.replaceFirst(String.valueOf(ogStr.charAt(i)), ""), output + ogStr.charAt(i));
-            }
-        }
-        '''
-
-            # new python based on translating java pseudocode
     if (len(string)==1):
         print(f"{passemot}{string}")
     else:
