6bb77778a8ad2cadbcdf39f40c507a3cb362eef6
vedantavagisa <vedantavagisa1486@gmail.com>
2026-08-17 16:50:18 -0400

    Polished Exercise 3
    Added code to take user input, and added header comments

--- Day 21 - Python fluency/Recursion/Recursion Exercise 3.py
+++ Day 21 - Python fluency/Recursion/Recursion Exercise 3.py
@@ -1,3 +1,9 @@
+'''
+Purpose:
+* Recreate the permutation exercise that I made in Java
+* This is the second branched recursion exercise
+'''
+
 def permutations(string, passemot):
 
     if (len(string)==1):
@@ -6,5 +12,5 @@ def permutations(string, passemot):
         for i in range(len(string)):
             permutations(string.replace(string[i], ""), passemot + string[i])
 
-string = "ABCD"
+string = input("What string do you want to reverse")
 permutations(string, "")
